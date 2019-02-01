95a2199a6130bd3b1210887a33e490eaf5261801
Rahulkarki17 <rahulkarki@hotmail.com>
2019-02-01 19:34:56 +0000

    first commit

---
+++ leadmanager/frontend/src/actions/types.js
@@ -0,0 +1,13 @@
+export const GET_LEADS = "GET_LEADS";
+export const DELETE_LEAD = "DELETE_LEAD";
+export const ADD_LEAD = "ADD_LEAD";
+export const GET_ERRORS = "GET_ERRORS";
+export const CREATE_MESSAGE = "CREATE_MESSAGE";
+export const USER_LOADING = "USER_LOADING";
+export const USER_LOADED = "USER_LOADED";
+export const AUTH_ERROR = "AUTH_ERROR";
+export const LOGIN_SUCESSS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+export const LOGOUT_SUCESSS = "LOGOUT_SUCCESS";
+export const REGISTER_SUCESSS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
